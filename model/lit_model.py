fb1f506a9cb7c15728f2ab5caa0734b125dc3fd2
nextflows <nextflows.ai@gmail.com>
2024-08-16 04:37:37 +0500

    Fix torchmetrics bug

--- model/lit_model.py
+++ model/lit_model.py
@@ -15,38 +15,38 @@ class LitClassifier(LightningModule):
         self.lr = learning_rate
         self.num_labels = num_labels
         self.save_hyperparameters()
-        
+
         self.model = ViTClf(n_labels=self.num_labels, model_path=model_path)
         self.criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
-        
-        self.train_accuracy = torchmetrics.Accuracy()
-        self.val_accuracy = torchmetrics.Accuracy()
+
+        self.train_accuracy = torchmetrics.Accuracy('binary')
+        self.val_accuracy = torchmetrics.Accuracy('binary')
         self.train_f1 = torchmetrics.F1Score(num_classes=self.num_labels, average="macro")
         self.val_f1 = torchmetrics.F1Score(num_classes=self.num_labels, average="macro")
-        
+
     def forward(self, x):
         x = self.model(x)
         # Convert the 4-d id classifier to non-passport and passport
         # x = torch.tensor([torch.sum(x[0][:3]), x[0][3]])
         return x
-    
+
     def common_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = self.criterion(logits, y)
         return loss, logits, y
-    
+
     def training_step(self, batch, batch_idx):
         loss, logits, y = self.common_step(batch, batch_idx)
         preds = torch.argmax(logits, dim=1)
         self.train_accuracy(preds, y)
         self.train_f1(preds, y)
-        
+
         self.log("train_loss", loss)
         self.log("train_acc", self.train_accuracy, prog_bar=True, on_epoch=True)
         self.log("train_f1", self.train_f1, prog_bar=True, on_epoch=True)
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         loss, logits, y = self.common_step(batch, batch_idx)
         preds = torch.argmax(logits, dim=1)
@@ -57,10 +57,10 @@ class LitClassifier(LightningModule):
         self.log("val_acc", self.val_accuracy, prog_bar=True, on_epoch=True, on_step=False)
         self.log("val_f1", self.val_f1, prog_bar=True, on_epoch=True, on_step=False)
         return loss
-    
+
     def test_step(self, batch, batch_idx):
         return self.validation_step(batch, batch_idx)
-    
+
     def configure_optimizers(self):
         optimizer = AdamW(self.parameters(), lr=self.lr)
         # lr_scheduler = optim.lr_scheduler.CosineAnnealingLR
